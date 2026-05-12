14c8e6f0bac51bed40f9d1223333177efc8a8e98
binalamimtiaz@gmail.com <binalamimtiaz@gmail.com>
2026-05-13 00:34:38 +0600

    books list section done

---
+++ controller/BorrowController.php
@@ -0,0 +1,18 @@
+<?php
+include '../model/Book.php';  
+
+class BorrowController
+{
+    public function showBooks(){
+        $books = getBooks();
+        
+        return $books;  
+    }
+}
+
+$controller = new BorrowController();   
+
+$books = $controller->showBooks();
+
+include_once '../view/member_books.php';    
+?>
